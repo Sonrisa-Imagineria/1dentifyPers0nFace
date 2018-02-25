4f564b9e574bde860f17a07eb28ef8b53863d049
Yvonne Shih <tailcloud14916@gmail.com>
2018-02-25 18:00:35 +0800

    classify cl0ckin.py

--- cl0ckin.py
+++ cl0ckin.py
@@ -52,29 +52,34 @@ class FaceIdentifier():
 
 # fider = FaceIdentifier()
 # fider.get_persons_from_image('test.jpg')
+class ClockIn():
+	video_capture = cv2.VideoCapture(0)
+	font                   = cv2.FONT_HERSHEY_SIMPLEX
+	bottomLeftCornerOfText = (10,250)
+	fontScale              = 1
+	fontColor              = (0,0,0)
+	lineType               = 2
+	persons_info = None
 
-video_capture = cv2.VideoCapture(0)
-font                   = cv2.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (10,250)
-fontScale              = 1
-fontColor              = (0,0,0)
-lineType               = 2
-print( 'WIDTH',video_capture.get(3),'HEIGHT',video_capture.get(4))
+	def __init__(self):
+		print( 'WIDTH',self.video_capture.get(3),'HEIGHT',self.video_capture.get(4))
+		self.video_capture.set(3,640)
+		self.video_capture.set(4,480)
+	def start(self):
+		while True:
+			ret, frame = self.video_capture.read()
+			if self.persons_info:
+				cv2.putText(frame,'Hello World!',
+			    self.bottomLeftCornerOfText,
+			    self.font,
+			    self.fontScale,
+			    self.fontColor,
+			    self.lineType)
+			cv2.imshow('Company Meeting Check In Sys', frame)
 
-video_capture.set(3,640)
-video_capture.set(4,480)
+			out = cv2.imwrite('capture.jpg', frame)
 
-while True:
-    ret, frame = video_capture.read()
-    cv2.putText(frame,'Hello World!',
-    bottomLeftCornerOfText,
-    font,
-    fontScale,
-    fontColor,
-    lineType)
-    cv2.imshow('Company Meeting Check In Sys', frame)
-
-    out = cv2.imwrite('capture.jpg', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break;
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+#TODO periodical polling thread for get_person_info
+clockrun = ClockIn()
+clockrun.start()
